2ee1e74993659102baf8b33398a2a2aabd55b53b
Henning Hasemann <henning@hasemail.de>
2016-12-17 11:10:40 +0100

    Removed redundancy in remote storage

--- src/harmony/remotes.py
+++ src/harmony/remotes.py
@@ -7,21 +7,40 @@ class Remotes(FileComponent):
 
     RELATIVE_PATH = 'remotes'
 
+    class Remote:
+        def __init__(self, id_, name, location):
+            self.id = id_
+            self.name = name
+            self.location = location
+
+        @classmethod
+        def from_dict(class_, d):
+            return class_(**d)
+
+        def to_dict(self):
+            return {
+                'id': self.id,
+                'name': self.name,
+                'location': self.location
+            }
+
+
+
     def __init__(self, path, by_name = None, by_id = None):
         super().__init__(path)
-        # TODO: we can do the indexing on loading, don't save duplicate
-        # information to file
         self.by_name = by_name if by_name else {}
         self.by_id = by_id if by_id else {}
-        #self.state = {
-            #'by_name': {},
-            #'by_id': {}
-        #}
+
+    @classmethod
+    def from_dict(class_, d):
+        by_name = { x['name']: Remote.from_dict(x) for x in d['remotes'] }
+        by_id = { x['id']: Remote.from_dict(x) for x in d['remotes'] }
+        return class_(d['path'], by_name, by_id)
 
     def to_dict(self):
+        l = list(set(self.by_name.values()) | set(self.by_id.values()))
         return {
-            'by_name': self.by_name,
-            'by_id': self.by_id,
+            'remotes': l,
         }
 
     def add(self, location, id_, name = None):
@@ -40,12 +59,10 @@ class Remotes(FileComponent):
 
     def get_location(self, id_ = None, name = None):
         if id_ is not None and id_ in self.by_id:
-            return selfby_id[id_]
+            return self.by_id[id_]
         if name is not None and name in self.by_name:
             return self.by_name[name]
         return None
-        #raise Exception('Location (id={}, name={}) not found.'
-                        #.format(id_, name))
 
     def get_locations(self):
         s = set(self.by_name.values())
